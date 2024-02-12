68f79e594df30c02fe37358635ba0b1635fe2d38
kusham <kusham1998@gmail.com>
2024-02-12 23:28:04 +0530

    add repository and controllers

---
+++ Controllers/GameController.cs
@@ -0,0 +1,56 @@
+﻿using GamesStore.API.Entities;
+using GamesStore.API.Repositories;
+
+namespace GamesStore.API.Controllers
+{
+    public static class GameController
+    {
+        const string _getGameEndpointName = "GetGame";
+
+        public static RouteGroupBuilder MapGamesControllers(this IEndpointRouteBuilder routes, GamesRepository
+            _gamesRepository)
+        {
+            var group = routes.MapGroup("/games").WithParameterValidation();
+
+            group.MapGet("/", () => _gamesRepository.GetAllGames());
+
+            group.MapGet("/{id:int}", (int id) =>
+            {
+                Game? game = _gamesRepository.GetGameById(id);
+                return game is not null ? Results.Ok(game) : Results.NotFound();
+            }).WithName(_getGameEndpointName);
+
+            group.MapPost("/", (Game game) =>
+            {
+                _gamesRepository.AddGame(game);
+                return Results.Created($"/games/{game.Id}", game);
+            }).WithName(_getGameEndpointName);
+
+            group.MapPut("/{id:int}", (int id, Game game) =>
+            {
+                if (id != game.Id)
+                {
+                    return Results.BadRequest("Id in the URL and in the body do not match");
+                }
+                if (_gamesRepository.GetGameById(id) is null)
+                {
+                    return Results.NotFound();
+                }
+                _gamesRepository.UpdateGame(game);
+                return Results.NoContent();
+            });
+
+            group.MapDelete("/{id:int}", (int id) =>
+            {
+                if (_gamesRepository.GetGameById(id) is null)
+                {
+                    return Results.NotFound();
+                }
+                _gamesRepository.DeleteGame(id);
+                return Results.NoContent();
+            });
+
+            return group;
+        }
+    }
+}
--- Entities/Game.cs
+++ Entities/Game.cs
@@ -1,12 +1,22 @@
-﻿namespace GamesStore.API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesStore.API.Entities
 {
     public class Game
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public required string Name { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public required string Genre { get; set; }
+        [Range(1, 100)]
         public decimal Price { get; set; }
         public DateTime ReleaseDate { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Url]
         public string? ImageUrl { get; set; }
     }
 }
